85e7981c1c3cefc3af811b36a3b174dd29a35a15
HyeonJeong <58041212+HyeonJeong519@users.noreply.github.com>
2022-10-08 23:19:07 +0900

    10/8

---
+++ src/main/java/service/MemberService.java
@@ -0,0 +1,35 @@
+package service;
+
+import domain.Member;
+import repository.MemberRepository;
+import repository.MemoryMemberRepository;
+
+import java.util.*;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //회원가입
+    public Long join(Member member){
+        //중복 회원 불가
+        vaildateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void vaildateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
