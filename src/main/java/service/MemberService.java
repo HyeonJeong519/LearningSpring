ac1d27c53d01891ff61cee69ad0efa6239241deb
HyeonJeong <58041212+HyeonJeong519@users.noreply.github.com>
2022-10-09 13:32:48 +0900

    10/9

--- src/main/java/service/MemberService.java
+++ src/main/java/service/MemberService.java
@@ -7,7 +7,11 @@ import repository.MemoryMemberRepository;
 import java.util.*;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
     //회원가입
     public Long join(Member member){
         //중복 회원 불가
