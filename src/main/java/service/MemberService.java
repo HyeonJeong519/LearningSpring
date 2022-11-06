441147c0fa1912695b7c4b7590527f92d03d4607
HyeonJeong <58041212+HyeonJeong519@users.noreply.github.com>
2022-11-06 11:02:53 +0900

    11/6

--- src/main/java/service/MemberService.java
+++ src/main/java/service/MemberService.java
@@ -2,6 +2,7 @@ package service;
 
 import domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import repository.MemberRepository;
 import repository.MemoryMemberRepository;
